8819113d8a23692880c30c4da99ca98da557caa6
Artemiy Ryabinkov <getlag@ya.ru>
2017-07-31 16:48:23 +0300

    Change NullAuthAdapter for implements interface
    Signed-off-by: Artemiy Ryabinkov <getlag@ya.ru>

--- src/Adapter/NullAuthenticatorAdapter.php
+++ src/Adapter/NullAuthenticatorAdapter.php
@@ -3,10 +3,14 @@
 namespace Furdarius\OIDConnect\Adapter;
 
 use Furdarius\OIDConnect\Contract\Authenticator;
+use Lcobucci\JWT\Token\DataSet;
 
 class NullAuthenticatorAdapter implements Authenticator
 {
-    public function authUserByEmail(string $email)
+    /**
+     * @param DataSet $claims
+     */
+    public function authUser(DataSet $claims)
     {
     }
 }
